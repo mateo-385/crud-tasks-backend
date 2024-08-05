1195475dd592b50cc7b3d98383aa0dd11fa6bbe0
Mateo <cuellamateo@gmail.com>
2024-08-05 16:12:20 -0300

    creacion de las carpetas y archivos

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+
+const app = express()
+const PORT = 3000
+
+//Middleware
+app.use(express.json())
+
+//Rutas
+app.get("/", (req, res) => {
+    res.send("Servidor funcionando")
+})
+
+app.use(require("./routes/tasks.routes"))
+
+app.listen(PORT, () => console.log(`Servidor funcionando en http://localhost:${PORT}`))
